9d735e02c01b4ae8ff1e9985b6b9ec2225c14a54
razewithlove <mister.timkinbro@mail.ru>
2022-11-09 02:04:34 +0500

    Добавьте файлы проекта.

---
+++ ToDoApp/Models/TodoModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    internal class TodoModel
+    {
+        public string _text;
+        private bool _isDone;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public bool IsDone 
+        { 
+            get { return _isDone; } 
+            set { _isDone = value; }
+        }
+        public string Text
+        { 
+            get { return _text; } 
+            set { _text = value; } 
+        }
+    }
+}
